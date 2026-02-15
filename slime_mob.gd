003f815c23f2e0dd8145b8bf38b13bbec1dd146b
dawwniel <niellann16@gmail.com>
2026-02-15 16:05:23 -0500

    added full working functionality to slime

--- slime_mob.gd
+++ slime_mob.gd
@@ -7,7 +7,7 @@ extends RigidBody3D
 var spd = randf_range(2.0, 4.0)
 var itemDropChance = 1#randi_range(1,2)
 var itemTypeChance = 4#randi_range(1,3)
-signal died
+#signal died
 
 @onready var player = get_node("/root/game/Player")
 
@@ -15,7 +15,7 @@ func _physics_process(_delta):
 	var horizontal_dir = Vector3(player.global_position.x - global_position.x, 1, player.global_position.z - global_position.z).normalized()
 	
 	linear_velocity.x = horizontal_dir.x * spd
-	linear_velocity.z = horizontal_dir.x * spd
+	linear_velocity.z = horizontal_dir.z * spd
 	linear_velocity.y = clamp(0, -spd, spd)
 	
 	var dist_to_player = global_position.distance_to(player.global_position)
@@ -44,34 +44,35 @@ func take_damage():
 				const PILLS = preload("res://ItemScenes/Pills.tscn")
 				var pills_item = PILLS.instantiate()
 				get_tree().current_scene.add_child(pills_item)
-				pills_item.global_position = %bat_model.global_position
-				pills_item.global_position.y -= 0.5
+				pills_item.global_position = %slime_model.global_position
+				pills_item.global_position += 0.8
 				
 			if(itemTypeChance == 2):
 				print("Dropped beans")
 				const BEANS= preload("res://ItemScenes/Beans.tscn")
 				var beans_item = BEANS.instantiate()
 				get_tree().current_scene.add_child(beans_item)
-				beans_item.global_position = %bat_model.global_position
-				beans_item.global_position.y -= 0.5
+				beans_item.global_position = %slime_model.global_position
+				beans_item.global_position += 0.8
 				
 			if(itemTypeChance == 3):
 				print("Dropped adrenaline")
 				const ADREN = preload("res://ItemScenes/Adrenaline.tscn")
 				var adren_item = ADREN.instantiate()
 				get_tree().current_scene.add_child(adren_item)
-				adren_item.global_position = %bat_model.global_position
-				adren_item.global_position.y -= 0.5
+				adren_item.global_position = %slime_model.global_position
+				adren_item.global_position += 0.8
 				
 			if(itemTypeChance == 4):
 				print("Dropped harpy feather")
 				const HARPY = preload("res://ItemScenes/HarpyFeather.tscn")
 				var harpy_item = HARPY.instantiate()
 				get_tree().current_scene.add_child(harpy_item)
-				harpy_item.global_position = %bat_model.global_position
-				harpy_item.global_position.y -= 0.5
+				harpy_item.global_position = %slime_model.global_position
+				harpy_item.global_position += 0.8
 				
 func split_on_death():
+	
 	const SLIME1 = preload("res://Scenes/slime_mob.tscn")
 	const SLIME2 = preload("res://Scenes/slime_mob.tscn")
 	const SLIME3 = preload("res://Scenes/slime_mob.tscn")
@@ -83,6 +84,10 @@ func split_on_death():
 	slime1.name = "slime1"
 	slime2.name = "slime2"
 	slime3.name = "slime3"
+	
+	get_tree().current_scene.add_child(slime1)
+	get_tree().current_scene.add_child(slime2)
+	get_tree().current_scene.add_child(slime3)
 	
 	slime1.global_position = %slime_model.global_position
 	slime1.global_position.x = %slime_model.global_position.x - 2
